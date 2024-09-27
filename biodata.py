db958fad24d08ad73cb2cc5ff9c84135ae6c57ca
stevenzektyramadhan <ldpo7471@gmail.com>
2024-09-27 10:51:53 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,13 @@
+nama_depan = "steven"
+nama_belakang = "ramadhan"
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = 18 #ini adalah string
+alamat = "permis"
+kata_mutiara = "coba aja dulu"
+
+
+print(nama_lengkap,  {usia}, ',' , 'dari' , alamat, ',kata mutiara:' , kata_mutiara)
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
